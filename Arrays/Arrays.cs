c0cf0b7b68673887034a86893bb0e7237db8529a
shubhamrawool <rawool.prasad@gmail.com>
2020-07-26 21:44:12 +0530

    Arrays

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    public class Arrays<T>
+    {
+        private T[] array = new T[0];
+        private int count = 0;
+
+        public void Insert(T value)
+        {
+            if (array.Length == count)
+            {
+                var newArray = new T[count == 0 ? 2 : count * 2];
+                for (int i = 0; i < count; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+            }
+            array[count++] = value;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if(index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            for (int i = index; i < count; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            count--;
+        }
+
+        public int IndexOf(T value)
+        {
+            for(var i=0; i < count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(array[i], value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Print()
+        {
+            for (var i = 0; i < count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays<int> value = new Arrays<int>();
+            value.Insert(10);
+            value.Insert(20);
+            value.Insert(30);
+            value.RemoveAt(1);
+            value.Insert(10);
+            Console.WriteLine($"The index of 10 is { value.IndexOf(10) }");
+            Console.WriteLine($"The Array is ");
+            value.Print();
+            Console.ReadLine();
+        }
+    }
+}
